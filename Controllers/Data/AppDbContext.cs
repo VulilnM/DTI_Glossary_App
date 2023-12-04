0d28957e1b86931666f505cd0ea37f92f9705479
vulin <vulinmb@gmail.com>
2023-12-04 22:13:33 +0100

    Connected to SQL server DB over EF core

---
+++ Controllers/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DTI_Glossary_App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DTI_Glossary_App.Controllers.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        
+        }
+
+        // Create table
+        public DbSet<Glossary> Glossaries { get; set; }
+        
+
+    }
+}
---
+++ Migrations/20231204210729_AddGlossaryTableToDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DTI_Glossary_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGlossaryTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Glossaries",
+                columns: table => new
+                {
+                    GlossaryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Term = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Definition = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Glossaries", x => x.GlossaryId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Glossaries");
+        }
+    }
+}
