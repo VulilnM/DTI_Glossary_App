194add78bc9cf07afd3b858ca0833c75da79cbad
vulin <vulinmb@gmail.com>
2023-12-04 18:46:04 +0100

    Defined  Glossary model structure

---
+++ Models/Glossary.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTI_Glossary_App.Models
+{
+    public class Glossary
+    {
+        [Key]
+        public int GlossaryId { get; set; }
+
+        [Required]
+        public string Term { get; set; }
+
+        [Required]
+        public string Definition { get; set; }
+    }
+}
