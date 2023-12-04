9d2fb85c939055c502474c77111b991f6a43d974
vulin <vulinmb@gmail.com>
2023-12-04 22:43:11 +0100

    Added dummy seed data to Db

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -13,6 +13,29 @@ namespace DTI_Glossary_App.Data
         // Create table
         public DbSet<Glossary> Glossaries { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Glossary>().HasData(
+            new Glossary
+            {
+                GlossaryId = 1,
+                Term = "Gloss1",
+                Definition = "Definition of the glossary number 1"
+            },
+            new Glossary
+            {
+                GlossaryId = 2,
+                Term = "Gloss2",
+                Definition = "Definition of the glossary number 2"
+            },
+            new Glossary
+            {
+                GlossaryId = 3,
+                Term = "Gloss3",
+                Definition = "Definition of the glossary number 3"
+            });
 
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ Migrations/20231204214202_SeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DTI_Glossary_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Glossaries",
+                columns: new[] { "GlossaryId", "Definition", "Term" },
+                values: new object[,]
+                {
+                    { 1, "Definition of the glossary number 1", "Gloss1" },
+                    { 2, "Definition of the glossary number 2", "Gloss2" },
+                    { 3, "Definition of the glossary number 3", "Gloss3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Glossaries",
+                keyColumn: "GlossaryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Glossaries",
+                keyColumn: "GlossaryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Glossaries",
+                keyColumn: "GlossaryId",
+                keyValue: 3);
+        }
+    }
+}
