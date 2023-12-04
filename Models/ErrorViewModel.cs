965d19943a3e8ddae906e9f627e9f2b8003e0c41
vulin <vulinmb@gmail.com>
2023-12-04 17:46:37 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DTI_Glossary_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
